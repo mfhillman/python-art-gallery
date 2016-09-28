d8ff071951689db4d723a928fb02591ba8a5c0e9
Matt Hillman <mfhillman@gmail.com>
2016-09-28 00:57:02 +0000

    Bug fixes

--- painting.py
+++ painting.py
@@ -3,7 +3,7 @@ from google.appengine.ext import blobstore
 from google.appengine.ext import ndb
 
 _PAINTING_BUCKET = 'mfh-art-gallery.appspot.com'
-_PAINTING_FORMAT = '/gs/' + _PAINTING_BUCKET + '/artwork-images/%s.jpg'
+_PAINTING_FORMAT = '/gs/' + _PAINTING_BUCKET + '/artwork-images/{0}.jpg'
 
 class Painting(ndb.Model):
 	file_name = ndb.StringProperty(indexed = False)
@@ -14,10 +14,11 @@ class Painting(ndb.Model):
 	def _image_path(self) :
 		return _PAINTING_FORMAT.format(self.file_name)
 
-	
+	def _blob_key(self) :
+		return blobstore.create_gs_key(self._image_path());
+		
 	def _base_image(self) :
-		blob_key = blobstore.create_gs_key(self._image_path());
-		return images.get_serving_url(blob_key)
+		return images.get_serving_url(self._blob_key())
 	
 	def full_size_image(self) :
 		return self._base_image() + '=s0'
