cd7029bc399a68faafd9aa9332b1f3776966dfd7
Matt Hillman <mfhillman@gmail.com>
2016-09-30 22:25:22 +0000

    Update before moving to beta

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+import datetime
+import os
+
+import jinja2
 import webapp2
 
-from home import HomeHandler
+from models import Painting
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+	extensions=['jinja2.ext.autoescape'],
+	autoescape=True)
+
+HOME_PAINTING = 'legendofsleepyhollow'
+
+class HomeHandler(webapp2.RequestHandler):
+    def get(self):
+   
+		year = datetime.datetime.now().year
+		header_painting = Painting(file_name=HOME_PAINTING)
+
+		template_values = {
+        	'year': year,
+        	'painting': header_painting
+		}		
+
+		template = JINJA_ENVIRONMENT.get_template('home.html')
+		self.response.write(template.render(template_values))
+        
 app = webapp2.WSGIApplication([
     ('/', HomeHandler)
 ], debug=True)
